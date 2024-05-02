8353cc98f22b2975d24e7f46462dc996d0455b93
Treasure56 <131236913+Treasure56@users.noreply.github.com>
2024-05-02 01:26:01 +0100

    updated: meta data

--- src/app/(public)/Hero.tsx
+++ src/app/(public)/Hero.tsx
@@ -4,7 +4,7 @@ import Technologies from "./Technologies";
 export default function HeroSection() {
   return (
     <section className="md:h-[80vh] max-h-[700px] relative overflow-hidden bg-[url(/images/hero-bg.png)]">
-      <div className="absolute bg-primary rounded-full blur-[50px] size-[500px] -top-40 -left-40"></div>
+      <div className="absolute bg-primary rounded-full blur-[100px] size-[500px] -top-40 -left-40"></div>
       <div className="absolute bg-secondary rounded-full blur-[50px]  size-[500px] -bottom-40 -right-40"></div>
       <div className=" max-md:flex-col flex bg-[#ffffffaa] backdrop-blur-[5px] app-container h-full w-full relative">
         <div className="flex flex-col max-md:items-center  md:h-full py-6 max-md:pt-20 justify-center flex-grow gap-2 w-full md:w-7/12">
