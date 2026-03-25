64b5de11f1c8a8684c77d57f79a2a9f548861073
treasure56 <simplicitytreasure@gmail.com>
2026-03-25 15:50:53 +0100

    improved UI

--- src/app/(public)/Hero.tsx
+++ src/app/(public)/Hero.tsx
@@ -9,12 +9,16 @@ export default function HeroSection() {
       <div className="absolute bg-secondary rounded-full blur-[50px]  size-[500px]  -top-40 -left-40"></div>
       <div className=" max-md:flex-col flex bg-[#ffffffaa] backdrop-blur-[5px] app-container h-full w-full relative">
         <div className="flex flex-col max-md:items-center  md:h-full py-6 max-md:pt-20 justify-center flex-grow gap-2 w-full md:w-7/12">
-          <p className="text-l font-bold">Hey there 👋, i&apos;m</p>
+          <p className="text-l font-bold">Hey there 👋, I&apos;m</p>
           <h1 className=" md:text-6xl text-3xl font-lilita">
             Treasure Onyenze
           </h1>
           <p className="md:text-2xl text-xl font-semibold">
             Web & Mobile App Engineer · AI Integration · UX-Focused
+          </p>
+          <p className="md:text-lg text-base font-semibold">
+            I build fast, beautiful apps that users love from fintech platforms
+            to AI-powered tools. Clean code. Thoughtful design. Real results
           </p>
           <div className="flex gap-3">
             <Link
